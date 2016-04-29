6c6186c3644bd542c9c610cb98ac7dc761fc5bb9
The Buildbot <buildbot@angular2buch.de>
2016-04-29 15:59:07 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/02ddd0c87510684bf56ea60bec13a107da807632
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/126690969

--- app/book-list/book-list.component.js
+++ app/book-list/book-list.component.js
@@ -1,33 +1,49 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var core_1 = require('angular2/core');
-var book_list_item_component_1 = require('../book-list-item/book-list-item.component');
-var book_1 = require('../domain/book');
-var BookListComponent = (function () {
-    function BookListComponent() {
-        this.books = [
-            new book_1.Book('9783864903571', 'Angular 2', ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'], new Date(2016, 5, 26), 'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen', 5, [new book_1.Thumbnail('http://goo.gl/nDi0Fc', 'Buchcover')], 'Dieses Buch vermittelt einen Schnelleinstieg in Angular 2...'),
-            new book_1.Book('9783864901546', 'AngularJS', ['Philipp Tarasiewicz', 'Robin Böhm'], new Date(2014, 5, 29), 'Eine praktische Einführung', 5, [new book_1.Thumbnail('https://goo.gl/Y5lFVE', 'Buchcover')], 'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts...')
-        ];
+System.register(['angular2/core', '../book-list-item/book-list-item.component', '../domain/book'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, book_list_item_component_1, book_1;
+    var BookListComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (book_list_item_component_1_1) {
+                book_list_item_component_1 = book_list_item_component_1_1;
+            },
+            function (book_1_1) {
+                book_1 = book_1_1;
+            }],
+        execute: function() {
+            BookListComponent = (function () {
+                function BookListComponent() {
+                    this.books = [
+                        new book_1.Book('9783864903571', 'Angular 2', ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'], new Date(2016, 5, 26), 'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen', 5, [new book_1.Thumbnail('http://goo.gl/nDi0Fc', 'Buchcover')], 'Dieses Buch vermittelt einen Schnelleinstieg in Angular 2...'),
+                        new book_1.Book('9783864901546', 'AngularJS', ['Philipp Tarasiewicz', 'Robin Böhm'], new Date(2014, 5, 29), 'Eine praktische Einführung', 5, [new book_1.Thumbnail('https://goo.gl/Y5lFVE', 'Buchcover')], 'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts...')
+                    ];
+                }
+                BookListComponent = __decorate([
+                    core_1.Component({
+                        selector: 'book-list',
+                        directives: [book_list_item_component_1.BookListItem],
+                        moduleId: __moduleName,
+                        templateUrl: 'book-list.component.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], BookListComponent);
+                return BookListComponent;
+            }());
+            exports_1("BookListComponent", BookListComponent);
+        }
     }
-    BookListComponent = __decorate([
-        core_1.Component({
-            selector: 'book-list',
-            directives: [book_list_item_component_1.BookListItem],
-            moduleId: module.id,
-            templateUrl: 'book-list.component.html'
-        }), 
-        __metadata('design:paramtypes', [])
-    ], BookListComponent);
-    return BookListComponent;
-}());
-exports.BookListComponent = BookListComponent;
+});
 //# sourceMappingURL=book-list.component.js.map
